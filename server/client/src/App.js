76af80bd04eaacf350a7901172e891eb90eae3f8
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-02-01 21:59:39 -0800

    minor change

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fuck off
+          Hi there
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/auth/google">sign In with Google</a>
       </header>
     </div>
   );
